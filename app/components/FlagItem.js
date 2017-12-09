19c5ae23e741d032e9358e8d4a0496f05eb82c56
Felix Rainer <felix.anton.rainer@gmail.com>
2017-12-09 15:36:20 +0100

    fixed flag bug, added nextactions array

--- app/components/FlagItem.js
+++ app/components/FlagItem.js
@@ -3,24 +3,38 @@
 import React, { Component } from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 import resolveAssetSource from 'resolveAssetSource';
-//import LeftAlignedImage from 'react-native-left-aligned-image';
 
 export default class FlagItem extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			newWidth: 0,
+			imageSize: undefined,
 		};
 	}
+	/*
+*	React automatically centers an image to the parent View when resizing
+*
+*/
+	componentWillReceiveProps = () => {
+		this.setState({ imageSize: undefined });
+	};
 
 	render() {
+		//console.log('FlagItem.js: current image size = ' + this.state.imageSize);
 		return (
-			<View style={[styles.flagContainer, this.props.style]}>
+			<View
+				style={
+					typeof this.state.imageSize !== 'undefined'
+						? [this.props.style, { width: this.state.imageSize.height * 1.57 }]
+						: [this.props.style, { opacity: 0 }]
+				}
+			>
 				<Image
 					style={
 						typeof this.state.imageSize !== 'undefined'
 							? [styles.image, this.state.imageSize]
-							: styles.image
+							: [styles.image, { opacity: 0 }]
 					}
 					source={this.props.flag.pic}
 					onLayout={event => {
@@ -40,13 +54,9 @@ export default class FlagItem extends Component {
 }
 
 const styles = StyleSheet.create({
+	//container: {},
 	image: {
 		height: '100%',
 		resizeMode: 'contain',
-	},
-	flagContainer: {
-		//flex: 1,
-		//backgroundColor: 'grey',
-		//flex: 1,
 	},
 });
